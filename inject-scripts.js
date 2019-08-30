3988a66bfdde9a22222bb1f807315cf185aad3e5
Vitali Borovik <2556969@gmail.com>
2019-08-30 16:44:15 +0300

    polish

--- inject-scripts.js
+++ inject-scripts.js
@@ -6,73 +6,68 @@ var _TALKABLE_DEMO_CONFIG = {
 
 document.addEventListener('DOMContentLoaded', function () {
 
-  var campaign_tag;
-
   var check_buttons = document.querySelectorAll('.integrate');
-
   var site_id = document.getElementById('site-id');
-  var campaign_tag_element = document.getElementById('campaign-tag');
 
 
   if (site_id) site_id.addEventListener('input', function(e) { _TALKABLE_DEMO_CONFIG.site_slug = e.target.value });
-  //if (campaign_tag_element) campaign_tag_element.document.getElementById('campaign-tag').addEventListener('input', function(e) { campaign_tag = e.target.value });
 
 
-  document.body.addEventListener('click', function(event) {
+  window.addEventListener('click', function(event) {
     console.log(event.target);
   });
 
   check_buttons.forEach(function (item) {
     item.addEventListener('click', function (event) {
-      campaign_tag = event.target.id;
 
-      chrome.tabs.executeScript({
-        file: 'library.js'
-      }, function() {
-        chrome.tabs.executeScript({
-          code: `
-          var _TALKABLE_DEMO_CONFIG = {
-            "site_slug": "${_TALKABLE_DEMO_CONFIG.site_slug}",            // real site_slug, i.e. for toms-v2 it should be toms
-            "version": "4.2.3",              // integration version
-            "without_integration_library": false // optional, false by default
-          };
-          
-          var div = document.createElement('div');
-          div.id = "talkable-offer";
-          div.style = "margin: 2.5% 0"
-          
-          var script = document.createElement('script');
-          script.src = \`https://d2jjzw81hqbuqv.cloudfront.net/integration/clients/${_TALKABLE_DEMO_CONFIG.site_slug}.min.js\`;
-          script.type = 'text/javascript';
+      chrome.tabs.executeScript({ file: 'library.js' });
+      chrome.tabs.executeScript({ code: `
+          if (!window.talkable_script) {
+            window.talkable_script = document.createElement('script');
+            window.talkable_script.src = "https://d2jjzw81hqbuqv.cloudfront.net/integration/talkable-4.2.min.js";
+            window.talkable_script.type = 'text/javascript';
+            document.body.appendChild(window.talkable_script);
+          }
           
           var api_calls = document.createElement('script');
           api_calls.type = 'text/javascript';
           api_calls.innerHTML = \`
+           window.talkablePlacementsConfig = { placements: ["true"] };
            window._talkableq = window._talkableq || [];
-            _talkableq.unshift(['init', { site_id: '${_TALKABLE_DEMO_CONFIG.site_slug}' }]);
-          
-            window._talkableq.push(['authenticate_customer', {
-              email: '', // Optional, pass when available. Example: 'customer@example.com'
-              first_name: '', // Optional, pass when available. Example: 'John'
-              last_name: '' // Optional, pass when available. Example: 'Smith'
-            }]);
-            ${event.target.id == "post-purchase" ? 
-              `window._talkableq.push(['register_purchase', {
+            _talkableq.unshift(["init", { site_id: "${_TALKABLE_DEMO_CONFIG.site_slug}", debug: true }]);
+
+            switch("${event.target.id}") {
+              case "post-purchase": 
+                window._talkableq.push(["register_purchase", {
                   purchase: {
-                    order_number: 'tkbl_generated_${new Date()}',
-                    subtotal: '${42}'
+                    order_number: "${new Date()}",
+                    subtotal: "${10}"
                   },
                   customer: {
-                    email: 'customer@sample.com' // Customer email address who issued a purchase. Example: 'customer@example.com'
+                    email: "customer@sample.com" // Customer email address who issued a purchase. Example: "customer@example.com"
                   }
-              }]);`
-              : `window._talkableq.push(['register_affiliate', { campaign_tags: ['${campaign_tag}']}]);`}\`
-          
-          
-          document.body.appendChild(div);
-          document.body.appendChild(script);
-          document.body.appendChild(api_calls);`
-        });
+                }]);
+                break;
+              case "invite": 
+                alert("please select container");
+                document.body.addEventListener("click", function(e) {
+                  if(e.target) {
+                    var div = document.createElement('div');
+                    div.id = "talkable-offer";
+                    div.style = "margin: 2.5% 0"
+                    e.target.appendChild(div);
+                    window._talkableq.push(["register_affiliate", { campaign_tags: ["invite"]}]);
+                  }
+                });
+                
+                break;
+              default: 
+                window._talkableq.push(["register_affiliate", { campaign_tags: ["${event.target.id}"]}]);
+            }
+          \`;
+                  
+          document.body.appendChild(api_calls);
+        `
       });
 
       // chrome.tabs.executeScript({
@@ -88,6 +83,4 @@ document.addEventListener('DOMContentLoaded', function () {
 
     });
   })
-
-
 });
